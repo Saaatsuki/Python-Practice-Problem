9514fd2a7c0e1c76a6120a2afddf7db8d1b2086a
Kawai <sasa46kaka@yahoo.co.jp>
2024-04-17 20:42:04 +0900

    PAIZA Practice

---
+++ PAIZA/1.py
@@ -0,0 +1,4 @@
+#　https://paiza.jp/works/mondai/d_rank_level_up_problems/d_rank_level_up_problems__stdout_1
+
+#　出力文を入力する
+print("paiza")
---
+++ PAIZA/2.py
@@ -0,0 +1,8 @@
+a , b = map(int,input().split())
+
+print(a * b)
+
+a = int(input())
+b = int(input())
+
+print(a * b)
