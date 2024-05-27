7fb8879b36523c036be4fc9cf3b4496c3c38c8ef
Saaatsuki <166793481+Saaatsuki@users.noreply.github.com>
2024-05-27 13:34:46 +0900

    Commit

---
+++ Programming-Class-Practice/240527/1.py
@@ -0,0 +1,7 @@
+num = int(input())
+
+if num %2 == 0:
+    print("짝수")
+
+elif num %2 != 0: # ✖ elif　→　〇 else
+    print("홍수")
---
+++ Programming-Class-Practice/240527/2.py
@@ -0,0 +1,32 @@
+import random
+import turtle
+
+screen = turtle.Screen()
+screen.title("Turtle 기보드 이벤트 처리 예제")
+
+t = turtle.Turtle()
+
+def move_forward():
+    t.forward(10)
+    
+def move_backward():
+    t.backward(10)
+    
+def turn_left():
+    t.left(15)
+
+def turn_right():
+    t.right(15)
+    
+def move_random():
+    colors = ["red","green","blue","yellow","purple","orange"]
+    t.pencolor(random.choice(colors))
+    
+screen.listen()
+screen.onkey(move_forward,"Up")
+screen.onkey(move_backward,"Down")
+screen.onkey(turn_left,"Left")
+screen.onkey(turn_right,"Right")
+screen.onkey(move_random,"c")
+
+screen.mainloop()
