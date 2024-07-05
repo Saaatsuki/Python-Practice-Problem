341031892a856977af0936c9442ce05d8442951b
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-07-05 19:19:55 +0900

    240705-4

---
+++ Programming-Class-Practice/2024 07/240705/5.py
@@ -0,0 +1,6 @@
+bar = [[10,20,30],[40,50],[60,70,80,90]]
+
+for row in bar:
+    for item in row:
+        print(item,end=" ")
+    print()
---
+++ Programming-Class-Practice/2024 07/240705/6.py
@@ -0,0 +1,34 @@
+def print_matrix(mat):
+    for row in mat:
+        print(row)
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+#2차원 리스트 행 추가
+matrix.append([10,11,12])
+print("InMatrix after adding a new row:")
+print_matrix(matrix)
+
+
+# 2차원 리스트 행 내 요소 추가 (새 열 추가)
+for row in matrix:
+    row.append(100) # 각 행의 끝에 100 추가
+print("InMatrix after adding a new column:")
+print_matrix(matrix)
+
+
+# 2차원 리스트 삭제 (Delete)
+del matrix[2] #세 번째 행 삭제
+print("InMatrix after deleting the third row:")
+print_matrix(matrix)
+
+
+# 특정 열 삭제 (예: 모든 행에서 두 번째 열 삭제)
+for row in matrix:
+    del row[1]  
+print("InMatrix after deleting the second column:")
+print_matrix(matrix)
