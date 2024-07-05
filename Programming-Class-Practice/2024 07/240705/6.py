6c717173411c62810a3578e12f70366be00889ce
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-07-05 19:27:07 +0900

    240705-5

--- Programming-Class-Practice/2024 07/240705/6.py
+++ Programming-Class-Practice/2024 07/240705/6.py
@@ -12,19 +12,21 @@ matrix = [
 matrix.append([10,11,12])
 print("InMatrix after adding a new row:")
 print_matrix(matrix)
-
+print()
 
 # 2차원 리스트 행 내 요소 추가 (새 열 추가)
 for row in matrix:
     row.append(100) # 각 행의 끝에 100 추가
 print("InMatrix after adding a new column:")
 print_matrix(matrix)
+print()
 
 
 # 2차원 리스트 삭제 (Delete)
 del matrix[2] #세 번째 행 삭제
 print("InMatrix after deleting the third row:")
 print_matrix(matrix)
+print()
 
 
 # 특정 열 삭제 (예: 모든 행에서 두 번째 열 삭제)
