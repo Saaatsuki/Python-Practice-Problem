d568ec162edce39df9db6fa172cb84702cfaa30f
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-07-09 20:57:38 +0900

    240709-6

--- Programming-Class-Practice/2024 07/240709/00lab1_2.py
+++ Programming-Class-Practice/2024 07/240709/00lab1_2.py
@@ -1,24 +1,10 @@
-def rank_elements(lst):
-    """
-    指定されたリスト内の各要素に対して順位をつける関数。
-    
-    パラメータ:
-        lst (list): 数字を含むリスト。
-    
-    戻り値:
-        list: 各要素の順位を示すリスト。
-    """
-    indexed_lst = list(enumerate(lst))
-    sorted_lst = sorted(indexed_lst, key=lambda x: x[1], reverse=True)
-    ranks = [0] * len(lst)
-    current_rank = 1
-    for i, (index, value) in enumerate(sorted_lst):
-        if i > 0 and value == sorted_lst[i - 1][1]:
-            ranks[index] = current_rank
-        else:
-            current_rank = i + 1
-            ranks[index] = current_rank
-    return ranks
+def sort_list_max_remove(lst):
+    sorted_list = []
+    while lst:
+        max_elem = max(lst)
+        sorted_list.append(max_elem)
+        lst.remove(max_elem)
+    return sorted_list
 
 menu = 0
 
@@ -28,6 +14,8 @@ student_file_eng = []
 student_file_math = []
 test_sum_file = []
 test_avg_file = []
+
+output_li = ["이름", "국어", "영어", "수학", "총점", "평균", "등수"]
 
 while menu != 4:
     print("\n[메뉴 선택]")
@@ -53,13 +41,20 @@ while menu != 4:
         test_avg_file.append(test_avg)
 
     elif menu == 2:
-        ranks = rank_elements(test_sum_file)
+        sorted_indices = sorted(range(len(test_sum_file)), key=lambda k: test_sum_file[k], reverse=True)
         print("[학생 성적 목록]")
-        print("{:<12} {:<5} {:<5} {:<5} {:<5} {:<7} {:<10}".format("이름", "국어", "영어", "수학", "총점", "평균", "등수"))
-        for i in range(len(student_file_name)):
-            print("{:<12} {:<5} {:<5} {:<5} {:<5} {:<7.2f} {:<10}".format(student_file_name[i], student_file_kokugo[i], student_file_eng[i], student_file_math[i], test_sum_file[i], test_avg_file[i], ranks[i]))
+        print("  ".join(output_li))
+        rank = 1
+        for i in sorted_indices:
+            print(f"{student_file_name[i]}  {student_file_kokugo[i]}  {student_file_eng[i]}  {student_file_math[i]}  {test_sum_file[i]}  {(test_avg_file[i]):.2f}  {rank}")
+            rank += 1
 
     elif menu == 3:
-        ranks = rank_elements(test_sum_file)
-        for i in range(len(student_file_name)):
-            print("{:<12} {:<5} {:<5} {:<5} {:<5} {:<7.2f} {:<10}".format(student_file_name[i], student_file_kokugo[i], student_file_eng[i], student_file_math[i], test_sum_file[i], test_avg_file[i], ranks[i]))
+        sorted_indices = sorted(range(len(test_sum_file)), key=lambda k: test_sum_file[k], reverse=True)
+        print("[학생 성적 파일 출력]")
+        rank = 1
+        for i in sorted_indices:
+            print(f"{student_file_name[i]}  {student_file_kokugo[i]}  {student_file_eng[i]}  {student_file_math[i]}  {test_sum_file[i]}  {(test_avg_file[i]):.2f}  {rank}")
+            rank += 1
+
+print("프로그램 종료")
