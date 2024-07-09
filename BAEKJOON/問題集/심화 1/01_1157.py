8d90349048458d8d2227172b24a58ad21ae197ec
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-07-09 21:26:12 +0900

    240709-10

--- BAEKJOON/問題集/심화 1/01_1157.py
+++ BAEKJOON/問題集/심화 1/01_1157.py
@@ -1,7 +1,11 @@
 word = input().upper()
-
+word_li = list(set(word))
 li = []
-for i in word:
-    li.append(word.count(i))
-print(li)
-print(word[li.index(max(li))])
+for i in word_li:
+    count = word.count(i)
+    li.append(count)
+
+if li.count(max(li))>=2:
+    print("?")
+else:
+    print(word_li[li.index(max(li))])
