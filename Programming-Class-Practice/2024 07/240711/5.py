eaf2835e758936853f777ecf778b7ca5c4807edf
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-07-11 20:40:08 +0900

    240711-9

--- Programming-Class-Practice/2024 07/240711/5.py
+++ Programming-Class-Practice/2024 07/240711/5.py
@@ -1,32 +1,53 @@
 from prettytable import PrettyTable
 
-person_di = {
-    "name" : "Kawai",
-    "age" : 24 ,
-    "city" : "New York"
+data_di = {
+    "name": None,
+    "age": None,
+    "city": None
 }
 
-print()
-print()
-for key in person_di:
-    print(f"Key : {key} , Value : {person_di[key]}")
+data_li = []
 
-print()
-for key , value in person_di.items():
-    print(f"Key : {key} , Value : {value}")
+while True:
+    name = input("\n이름은 입력 : ")
+    if name == "1":
+        break
+    age = int(input("나이를 입력 : "))
+    city = input("출신 도시를 입력 : ")
 
-print()
-for key in person_di.keys():
-    print(f"Key : {key}")
+    data_di = {
+    "name": name,
+    "age": age,
+    "city": city
+    }
+    data_li.append(data_di)
 
-print()
-for value in person_di.values():
-    print(f"Value : {value}")
-
-print()
 table = PrettyTable()
 table.field_names = ["name" ,"age" , "city"]
-table.add_row([
-    person_di["name"] , f'{person_di["age"]} 명' , person_di["city"]
-])
+for dic in data_li: 
+    table.add_row([dic["name"], dic["age"], dic["city"]])
 print(table)
+
+# print()
+# print()
+# for key in person_di_1:
+#     print(f"Key : {key} , Value : {person_di_1[key]}")
+
+# print()
+# for key , value in person_di_1.items():
+#     print(f"Key : {key} , Value : {value}")
+
+# print()
+# for key in person_di_1.keys():
+#     print(f"Key : {key}")
+
+# print()
+# for value in person_di_1.values():
+#     print(f"Value : {value}")
+
+# print()
+# table = PrettyTable()
+# table.field_names = ["name" ,"age" , "city"]
+# for dic in data_li: 
+#     table.add_row([dic["name"], dic["age"], dic["city"]])
+# print(table)
