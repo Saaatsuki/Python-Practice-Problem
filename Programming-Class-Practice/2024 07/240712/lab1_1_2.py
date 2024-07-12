e96f6bd31dc1c9ac0baf0316d95c5c723068586f
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-07-12 19:14:02 +0900

    240712-4

---
+++ Programming-Class-Practice/2024 07/240712/lab1_1_2.py
@@ -0,0 +1,11 @@
+import csv
+
+with open("2024_std_num_high_school.csv",newline="",encoding="utf-8-sig") as f:
+    reader = csv.DictReader(f)
+
+    se = set()
+    for row in reader:
+        se.add(row["시도교육청"])
+
+    for idx , val in enumerate(se):
+        print(f"{(idx+1):2d} : {val}")
