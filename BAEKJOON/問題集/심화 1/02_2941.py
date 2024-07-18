d8b1aff3140626a6886944d67ffac6dcadc13544
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-07-18 18:32:56 +0900

    240718-2

--- BAEKJOON/問題集/심화 1/02_2941.py
+++ BAEKJOON/問題集/심화 1/02_2941.py
@@ -1,15 +1,26 @@
-def count_alp(argWord):
-    croa_alp = ["c=","c-","dz=","d-","lj","nj","s=","z="]
-    count = 0
-    for alp in croa_alp:
-        while alp in argWord:
-            argWord = argWord.replace(alp,"",1)
-            count += 1
+# def count_alp(argWord):
+#     croa_alp = ["c=","c-","dz=","d-","lj","nj","s=","z="]
+#     count = 0
+#     for alp in croa_alp:
+#         while alp in argWord:
+#             argWord = argWord.replace(alp,"",1)
+#             count += 1
     
-    argWord = argWord.replace(" ","")
-    count += len(argWord)
+#     argWord = argWord.replace(" ","")
+#     count += len(argWord)
 
-    return count
+#     return count
 
-word = input().strip()
-print(count_alp(word))
+# word = input().strip()
+# print(count_alp(word))
+
+def count_croatian_alphabets(word):
+    croatian_alphabets = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+    
+    for alphabet in croatian_alphabets:
+        word = word.replace(alphabet, "*")
+    
+    print(len(word))
+
+word = input()
+count_croatian_alphabets(word)
