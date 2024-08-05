fe2820c61b1cd640e4c463d4d65ab7d27edd26a7
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-08-05 18:04:41 +0900

    240805-5

---
+++ Chulmun/복습/24_08/240805/20.py
@@ -0,0 +1,15 @@
+while True:
+    print("-"*30)
+    print("1) 구구단 출력")
+    print("2) 랜덤값 삼각형 출력")
+    print("3) 중료")
+    print("-"*30)
+
+    menu = int(input())
+    # while not 1<=menu<=3:
+
+    #     if menu == 1:
+    #         gugu = input()
+    #         if "~" in gugu:
+                
+    
