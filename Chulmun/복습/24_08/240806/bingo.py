df0e8c4b7a88c2a4e3abf165b75f3647a04799fa
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-08-07 15:22:02 +0900

    240807-3

--- Chulmun/복습/24_08/240806/bingo.py
+++ Chulmun/복습/24_08/240806/bingo.py
@@ -5,18 +5,51 @@ def output_bingo(argList, masu):
         for j in range(masu):
             print(f"{argList[i * masu + j]:2}", end=" ")
         print()
-        
+
+def count_bingos(bingo_list, size):
+    bingos = 0
+
+    # Check rows and columns
+    for i in range(size):
+        if all(bingo_list[i * size + j] == " *" for j in range(size)):
+            bingos += 1
+        if all(bingo_list[j * size + i] == " *" for j in range(size)):
+            bingos += 1
+
+    # Check diagonals
+    if all(bingo_list[i * size + i] == " *" for i in range(size)):
+        bingos += 1
+    if all(bingo_list[i * size + (size - 1 - i)] == " *" for i in range(size)):
+        bingos += 1
+
+    return bingos
 
 masu = int(input("3~6의 숫자를 입력해주세요 : "))
-while not 3<=masu<=6:
+while not 3 <= masu <= 6:
     masu = int(input("3~6의 숫자를 입력해주세요 : "))
 
 bingo_num_li_len = masu * masu
 bingo_num_li = []
 
 while len(bingo_num_li) < bingo_num_li_len:
-    bingo_num = random.randint(1,36)
+    bingo_num = random.randint(1, 36)
     if bingo_num not in bingo_num_li:
         bingo_num_li.append(bingo_num)
 
-output_bingo(bingo_num_li,masu)
+print("\n👾BINGO👾")
+output_bingo(bingo_num_li, masu)
+
+bingo_cou = 0
+while bingo_cou < 3:
+    num = int(input("\n숫자를 입력해주세요 : "))
+    while num not in bingo_num_li:
+        num = int(input("BINGO안에 입력한 숫자 없습니다.\n다시 숫자를 입력해주세요 : "))
+
+    idx = bingo_num_li.index(num)
+    bingo_num_li[idx] = " *"
+    output_bingo(bingo_num_li, masu)
+    
+    bingo_cou = count_bingos(bingo_num_li, masu)
+    print(f"\n현재 빙고 수: {bingo_cou}")
+
+print("\n3개의 빙고를 완성했습니다! 게임을 종료합니다.")
