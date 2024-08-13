dcba9b9ee6ecaed1dec27e8ff63ebfa76940f0cf
Kawai Satsuki <sasa46kaka@yahoo.co.jp>
2024-08-13 16:34:04 +0900

    240813-1

---
+++ Programming-Class-Practice/2024 08/240813/1.py
@@ -0,0 +1,53 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
+
+# 플레이어 사각형 정의
+player_rect = pygame.Rect(0, 0, 80, 40)
+
+
+# 객체 이동 속도
+speed = 300 # 300 pixel / second
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    dt = clock.tick(60) / 1000 # dt 프로그램 실행 멈춤    
+
+   
+    keys = pygame.key.get_pressed()
+    
+    # 왼쪽 방향키가 눌러졌을 때
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= speed * dt      
+
+    # 오른쪽 방향키가 눌러졌을 때
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += speed * dt 
+        
+    
+    # 화면 경계 처리    
+    player_rect.x = max(0, min(player_rect.x, screen.get_width() - player_rect.width))
+    player_rect.x = max(0, min(player_rect.x, screen.get_height() - player_rect.height))
+
+ 
+    # 화면을 흰색으로 칠한다.
+    screen.fill((255, 255, 255))
+   
+    # 플레이어 사각형 그리기         
+    pygame.draw.rect(screen, (0, 0, 255), player_rect) 
+   
+    
+    # 지금까지 메모리에 작성된 그림을 화면(Screen)에 출력
+    pygame.display.flip()
+    
+    
+pygame.quit()
+
+
---
+++ Programming-Class-Practice/2024 08/240813/3.py
@@ -0,0 +1,85 @@
+import pygame
+
+# 스페이스 키를 누르면 총알이 발사된다.
+'''
+1. 스페이스 키를 누르면
+2. 총알 사각형을 생성
+3. 2번에서 생성된 사각형을 플레이어 사각형 Top 중앙에 위치
+4. 매 Frame(image) 마다 총알의 y 좌표를 감소 (아래에서 위로 이동)
+5. 총알이 화면(Screen) 상단(TOP)을 넘어가면 총알을 삭제
+'''
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+running = True
+
+# 환경 변수
+p_rect_width = 80
+p_rect_height = 40
+f_rect_width = 30
+f_rect_height = 30
+
+# 플레이어 사각형 정의
+p_rect_x = screen_width // 2 - p_rect_width // 2
+p_rect_y = screen_height - p_rect_height - 5
+player_rect = pygame.Rect(p_rect_x, p_rect_y, p_rect_width, p_rect_height)
+
+
+# 객체 이동 속도
+speed = 300 # 300 pixel / second
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    dt = clock.tick(60) / 1000 # dt 프로그램 실행 멈춤    
+
+   
+    keys = pygame.key.get_pressed()
+    
+    # 왼쪽 방향키가 눌러졌을 때
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= speed * dt      
+
+    # 오른쪽 방향키가 눌러졌을 때
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += speed * dt 
+        
+        
+    # 스페이키가 눌러졌을 때
+    #if keys[pygame.K_SPACE]:
+    # 총알 사각형을 생성하고
+    fire_rect = pygame.Rect(0, 0, f_rect_width, f_rect_height)
+    fire_rect.bottom = player_rect.top
+    fire_rect.x = player_rect.centerx - f_rect_width // 2
+    
+    # 생성된 사각형을 플레이어 사각형 Top 중앙에 위치
+        
+    
+    # 화면 경계 처리    
+    player_rect.x = max(0, min(player_rect.x, screen.get_width() - player_rect.width))
+
+
+ 
+    # 화면을 흰색으로 칠한다.
+    screen.fill((255, 255, 255))
+   
+    # 플레이어 사각형 그리기         
+    pygame.draw.rect(screen, (0, 0, 255), player_rect) 
+   
+    # 총알 그리기
+    pygame.draw.rect(screen, (255, 0, 0), fire_rect) 
+    
+    # 지금까지 메모리에 작성된 그림을 화면(Screen)에 출력
+    pygame.display.flip()
+    
+    
+pygame.quit()
+
+
