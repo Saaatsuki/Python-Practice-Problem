82d1ec29a4fb96a7025424547529c88551a066a2
Saaatsuki <sasa46kaka@g.yju.ac.kr>
2024-09-06 21:38:48 +0900

    240906

--- BAEKJOON/LEVEL/Silver 2/11_10989.py
+++ BAEKJOON/LEVEL/Silver 2/11_10989.py
@@ -1,11 +1,73 @@
-T = int(input())
+# T = int(input())
 
-li = []
-for _ in range(T):
-    num = int(input())
-    li.append(num)
+# li = []
+# for _ in range(T):
+#     num = int(input())
+#     li.append(num)
 
-li.sort()
+# li.sort()
 
 
-print("\n".join(map(str,li)))
+# print("\n".join(map(str,li)))
+
+
+# import sys
+
+# T = int(input()) 
+# li = []
+# for _ in range(T):
+#     num = int(sys.stdin.readline())  
+#     li.append(num)
+
+# li.sort()
+# print("\n".join(map(str, li)))
+
+# T = int(input())
+
+# li = []
+# for _ in range(T):
+#     num = int(input())
+#     li.append(num)
+
+# li.sort()
+
+
+# print("\n".join(map(str,li)))
+
+
+# import sys
+
+# T = int(input()) 
+# li = []
+# for _ in range(T):
+#     num = int(sys.stdin.readline())  
+#     li.append(num)
+
+# li.sort()
+# print("\n".join(map(str, li)))
+
+# import sys
+
+# T = int(input()) 
+# li = []
+# for _ in range(T):
+#     num = int(sys.stdin.readline())
+#     li.append(num)
+
+# for num in sorted(li):
+#     print(num)
+
+
+# import sys
+# import heapq
+
+# T = int(input()) 
+# min_heap = []  
+
+# for _ in range(T):
+#     num = int(sys.stdin.readline())
+#     heapq.heappush(min_heap, num)  
+
+# while min_heap:
+#     print(heapq.heappop(min_heap))
+
